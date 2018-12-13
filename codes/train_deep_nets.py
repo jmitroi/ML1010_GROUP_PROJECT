dfe41cba97f22fc75e55384d590ee6c9220463b6
James Lai <gdlaizheng@gmail.com>
2018-12-12 23:58:17 -0500

    remove under sampling

--- codes/train_deep_nets.py
+++ codes/train_deep_nets.py
@@ -43,7 +43,7 @@ def main():
     df = pd.read_csv("../data/normalized_texts_labels.csv")
     df = df[["normalized_text", "fake"]]
     df.columns = ["texts", "labels"]
-    df = df.iloc[list(range(0,df.shape[0],10))]
+    #df = df.iloc[list(range(0,df.shape[0],10))]
     print("# of NaN of text:" + str(df["texts"].isnull().sum()))
     print("# of NaN of label:" + str(df["labels"].isnull().sum()))
     df = df.dropna()
